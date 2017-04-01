ac4e0ac02f99b7b51a002f067cb4c3023513c062
Kishidle <arcaramon@gmail.com>
2017-04-02 02:13:50 +0800

    tygjn'

--- QUEST/src/view/Profile.java
+++ QUEST/src/view/Profile.java
@@ -7,6 +7,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import net.miginfocom.swing.MigLayout;
+import java.awt.Color;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
 
 public class Profile extends JFrame {
 
@@ -33,11 +39,53 @@ public class Profile extends JFrame {
 	 */
 	public Profile() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 399, 261);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(null);
+		
+		JLabel lblTest = new JLabel("");
+		lblTest.setOpaque(true);
+		lblTest.setForeground(Color.BLACK);
+		lblTest.setBackground(Color.WHITE);
+		lblTest.setBounds(10, 22, 114, 99);
+		contentPane.add(lblTest);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(153, 22, 66, 14);
+		contentPane.add(lblUsername);
+		
+		JTextPane userNameTextPane = new JTextPane();
+		userNameTextPane.setBounds(218, 22, 149, 14);
+		contentPane.add(userNameTextPane);
+		
+		JLabel lblBadgesCollected = new JLabel("Badges Collected:");
+		lblBadgesCollected.setBounds(153, 47, 95, 14);
+		contentPane.add(lblBadgesCollected);
+		
+		JTextPane badgesCollectedTextPane = new JTextPane();
+		badgesCollectedTextPane.setBounds(249, 47, 58, 14);
+		contentPane.add(badgesCollectedTextPane);
+		
+		JLabel levelLabel = new JLabel("Level:");
+		levelLabel.setBounds(153, 72, 46, 14);
+		contentPane.add(levelLabel);
+		
+		JLabel levelNumberLabel = new JLabel("");
+		levelNumberLabel.setBounds(187, 72, 46, 14);
+		contentPane.add(levelNumberLabel);
+		
+		JLabel lblExpTillNext = new JLabel("EXP till next level:");
+		lblExpTillNext.setBounds(153, 97, 107, 14);
+		contentPane.add(lblExpTillNext);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(249, 97, 58, 14);
+		contentPane.add(textPane);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(284, 188, 89, 23);
+		contentPane.add(btnBack);
 	}
-
 }
