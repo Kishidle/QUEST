86d0b2c409d4e21997c97702048951997b9351ba
Kishidle <arcaramon@gmail.com>
2017-04-03 20:34:42 +0800

    leaderboard

--- QUEST/src/view/LeaderboardMenu.java
+++ QUEST/src/view/LeaderboardMenu.java
@@ -3,10 +3,17 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
 
 public class LeaderboardMenu {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,42 @@ public class LeaderboardMenu {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblLeaderboards = new JLabel("Leaderboards");
+		lblLeaderboards.setBounds(184, 11, 78, 14);
+		panel.add(lblLeaderboards);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"Name", "Ranking"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, Integer.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setResizable(false);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.setBounds(10, 36, 414, 80);
+		panel.add(table);
+		
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.setBounds(335, 227, 89, 23);
+		panel.add(btnConfirm);
 	}
-
 }
