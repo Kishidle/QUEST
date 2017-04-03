3efc75cea4676981a35312e1b77a747864f77abd
Kishidle <arcaramon@gmail.com>
2017-04-04 00:20:45 +0800

    password field

---
+++ QUEST/src/view/BadgeMenuTest.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class BadgeMenuTest {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BadgeMenuTest window = new BadgeMenuTest();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BadgeMenuTest() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
--- QUEST/src/view/Login.java
+++ QUEST/src/view/Login.java
@@ -28,6 +28,7 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPasswordField;
 
 /**
  * The Login Screen of the program.
@@ -37,8 +38,8 @@ public class Login extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField fNum;
-	private JTextField fPassword;
 	private JLabel lLogin;
+	private JPasswordField fPassword;
 
 	/**
 	 * Launch the application.
@@ -80,9 +81,6 @@ public class Login extends JFrame {
 
 		JLabel lPassword = new JLabel("Password");
 
-		fPassword = new JTextField();
-		fPassword.setColumns(10);
-
 		lLogin = new JLabel("QUEST - Login");
 		lLogin.setFont(new Font("Tahoma", Font.PLAIN, 24));
 
@@ -94,7 +92,8 @@ public class Login extends JFrame {
 					public void run() {
 						try {
 							String username = fNum.getText();
-							String password = fPassword.getText();
+							String password = String.valueOf(fPassword.getPassword());
+							System.out.print(password);
 
 							Connection conn = null;
 							Statement stmt = null;
@@ -145,47 +144,48 @@ public class Login extends JFrame {
 				});
 			}
 		});
+		
+		fPassword = new JPasswordField();
 
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-						.addContainerGap(76, Short.MAX_VALUE)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-												.addComponent(lNum)
-												.addComponent(lPassword))
-										.addGap(18)
-										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-												.addComponent(fPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-												.addComponent(fNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addGap(17)
-										.addComponent(lLogin))
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addGap(55)
-										.addComponent(btnConfirm)))
-						.addGap(73))
-				);
-		gl_contentPane.setVerticalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-						.addGap(22)
-						.addComponent(lLogin)
-						.addGap(34)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+					.addContainerGap(100, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addComponent(lNum)
-								.addComponent(fNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGap(18)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lPassword)
-								.addComponent(fPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGap(40)
-						.addComponent(btnConfirm)
-						.addContainerGap(23, Short.MAX_VALUE))
-				);
+								.addComponent(lPassword))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(fPassword)
+								.addComponent(fNum)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(17)
+							.addComponent(lLogin))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(55)
+							.addComponent(btnConfirm)))
+					.addGap(73))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(22)
+					.addComponent(lLogin)
+					.addGap(34)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lNum)
+						.addComponent(fNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lPassword)
+						.addComponent(fPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(40)
+					.addComponent(btnConfirm)
+					.addContainerGap(35, Short.MAX_VALUE))
+		);
 		contentPane.setLayout(gl_contentPane);
 	}
-
 }
