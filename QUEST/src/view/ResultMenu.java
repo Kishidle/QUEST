429e13dbb3ac0d1c23ff7e4a8add24871ccd26f5
Kishidle <arcaramon@gmail.com>
2017-04-03 15:00:15 +0800

    test menu stuff

--- QUEST/src/view/ResultMenu.java
+++ QUEST/src/view/ResultMenu.java
@@ -42,7 +42,7 @@ public class ResultMenu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 389);
+		frame.setBounds(100, 100, 450, 385);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
--- QUEST/src/view/TestMenu.java
+++ QUEST/src/view/TestMenu.java
@@ -3,10 +3,18 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class TestMenu {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,8 +44,36 @@ public class TestMenu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 385);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblDescription = new JLabel("Description:");
+		lblDescription.setBounds(10, 24, 67, 14);
+		panel.add(lblDescription);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 69, 414, 168);
+		panel.add(scrollPane);
+		
+		JTextArea txtrCodeArea = new JTextArea();
+		txtrCodeArea.setText("code area");
+		scrollPane.setViewportView(txtrCodeArea);
+		
+		JLabel lblAnswer = new JLabel("Answer:");
+		lblAnswer.setBounds(10, 251, 46, 14);
+		panel.add(lblAnswer);
+		
+		textField = new JTextField();
+		textField.setBounds(55, 248, 369, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(335, 312, 89, 23);
+		panel.add(btnSubmit);
 	}
-
 }
